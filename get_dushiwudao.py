2f6d633e23950b3acbeccc9aa8886272d95e0fe5
shi2006 <shi_2006@163.com>
2019-10-02 08:44:30 +0800

    version1.1.0

--- get_dushiwudao.py
+++ get_dushiwudao.py
@@ -1,12 +1,13 @@
 # *-* coding=utf-8 *-*
 
-import requests,os,re,time
+import requests, os, re, time
 from lxml import etree
+
 
 def request_url(url, path):
     """下载并保存网页"""
     headers = {
-        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"
     }
 
     response = requests.get(url, headers=headers)
@@ -18,7 +19,7 @@ def request_url(url, path):
 def get_article(page_path, dir):
     """对 page 页处理
     从里面的获取文章链接及标题
-    然后通过 requests_quest() 下载保存"""
+    然后通过 request_url() 下载保存"""
 
     # 用 XPath 来提取 page 页中的文章链接及标题
     parser = etree.HTMLParser(encoding="utf-8")
@@ -42,16 +43,15 @@ def get_article(page_path, dir):
         # 所以要换成中文的冒号, 这里用正则表达式来替换
         titles[i] = re.sub(r":", "：", titles[i])
 
-        article_name = titles[i]+".html"
+        article_name = titles[i] + ".html"
         # 判断文章是否已经下载，没有才会下载
         if article_name not in article_name_lists:
-            request_url(link, dir+article_name)
+            request_url(link, dir + article_name)
         i += 1
         time.sleep(1)
 
 
 def main():
-
     # 创建dushiwudao目录
     dir = "./dushiwudao/"
     if not os.path.exists(dir):
@@ -60,12 +60,12 @@ def main():
     # 获取每一 page 网页并下载保存
     # 至2019-09-09日，只有6页
 
-    for i in range(1,7):
+    for i in range(1, 7):
         # 分页 page 的 url
-        page_url = "http://www.dushiwudao.com/page/"+str(i)
+        page_url = "http://www.dushiwudao.com/page/" + str(i)
 
         # page 的保存路径
-        page_path = dir+"page"+str(i)+".html"
+        page_path = dir + "page" + str(i) + ".html"
 
         # 下载并保存 page 页，在爬的时候，暂时没有出现异常，因此暂时没有处理异常的步骤
         request_url(page_url, page_path)
@@ -73,8 +73,7 @@ def main():
 
         # 下载完成 page 页后，对 page 页处理
         # 从里面的获取文章链接及标题
-        # 然后通过 requests.get 访问链接，并保存
-        get_article(page_path,dir)
+        get_article(page_path, dir)
 
 
 if __name__ == '__main__':
